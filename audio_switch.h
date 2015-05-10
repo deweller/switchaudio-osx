81a6d5a562fca27db8ca962625ae591bf516e025
agiz <ziga.zupanec@gmail.com>
2015-05-10 18:40:37 +0200

    Code refactoring, API update and added functionality to change volume.

--- audio_switch.h
+++ audio_switch.h
@@ -4,6 +4,7 @@
  *
 
 Copyright (c) 2008 Devon Weller <wellerco@gmail.com>
+Copyright (c) 2015 Ziga Zupanec <ziga.zupanec@gmail.com>
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
@@ -26,33 +27,32 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 
- *
- */
+*/
 
 #include <unistd.h>
 #include <CoreServices/CoreServices.h>
 #include <CoreAudio/CoreAudio.h>
+#include <CoreAudio/AudioHardwareBase.h>
 
-
-typedef enum { 
-  	kAudioTypeUnknown = 0, 
-    kAudioTypeInput   = 1, 
-    kAudioTypeOutput  = 2,
-    kAudioTypeSystemOutput = 3
+typedef enum {
+  kAudioTypeUnknown = 0,
+  kAudioTypeInput   = 1,
+  kAudioTypeOutput  = 2,
+  kAudioTypeSystemOutput = 3
 } ASDeviceType;
 
 enum {
-	kFunctionSetDevice   = 1,
-	kFunctionShowHelp    = 2,
-	kFunctionShowAll     = 3,
-	kFunctionShowCurrent = 4,
-	kFunctionCycleNext   = 5
+  kFunctionSetDevice   = 1,
+  kFunctionShowHelp  = 2,
+  kFunctionShowAll   = 3,
+  kFunctionShowCurrent = 4,
+  kFunctionCycleNext   = 5,
+  kFunctionSetVolume   = 6
 };
-
-
 
 void showUsage(const char * appName);
 int runAudioSwitch(int argc, const char * argv[]);
+void printProperties(AudioDeviceID deviceID, ASDeviceType device_type);
 AudioDeviceID getCurrentlySelectedDeviceID(ASDeviceType typeRequested);
 void getDeviceName(AudioDeviceID deviceID, char * deviceName);
 ASDeviceType getDeviceType(AudioDeviceID deviceID);
@@ -64,4 +64,4 @@ AudioDeviceID getRequestedDeviceID(char * requestedDeviceName, ASDeviceType type
 AudioDeviceID getNextDeviceID(AudioDeviceID currentDeviceID, ASDeviceType typeRequested);
 void setDevice(AudioDeviceID newDeviceID, ASDeviceType typeRequested);
 void showAllDevices(ASDeviceType typeRequested);
-
+void setDeviceVolume(AudioDeviceID deviceID, float vol_left, float vol_right);
